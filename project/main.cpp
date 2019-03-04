8e09448631fabae2c2595014f86d79bc2a565389
u211-03 <u211-03@public.mpei.local>
2019-03-04 12:06:00 +0300

    file:максимум

--- project/main.cpp
+++ project/main.cpp
@@ -7,8 +7,19 @@ int main()
     cout << "Enter A and B:\n";
     int a,b;
     cin>>a>>b;
-cout << "A + B = " << a + b << '\n'
-     << "A - B = " << a - b << '\n';
-cout<<"A*B="<<a*b;
-cout<<"A/B="<<a/b;
+    cout << "A + B = " << a + b << '\n'
+         << "A - B = " << a - b << '\n'
+         << "A * B = " << a * b << '\n'
+         << "A / B = " << a / b << '\n';
+    int c;
+    if(a>b)
+    {
+        c=a;
+        cout<<"MAX="<<c;
+    }
+    else
+    {
+        c=b;
+        cout<<"MAX="<<c;
+    }
 }
